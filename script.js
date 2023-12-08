a269cf6da5281515ae73a86918b18156d2f66b32
aspiramedia <aspiramedia@gmail.com>
2023-12-08 17:49:20 +0000

    Rudimentary crust exclusion, options checkboxes and caveats.

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 // Function to compare areas and update the result
 function compareAreas() {
     // Get the diameter and quantity input values for both pizzas
-    const diameter1 = parseFloat(document.getElementById("diameterInput1").value);
+    let diameter1 = parseFloat(document.getElementById("diameterInput1").value);
     const quantity1 = parseInt(document.getElementById("quantityInput1").value, 10);
     
-    const diameter2 = parseFloat(document.getElementById("diameterInput2").value);
+    let diameter2 = parseFloat(document.getElementById("diameterInput2").value);
     const quantity2 = parseInt(document.getElementById("quantityInput2").value, 10);
+    
+    // Extra options
+    const excludeCrust = document.getElementById("excludeCrust").checked;
 
     // Check if the inputs are valid numbers and quantities
     if (!isNaN(diameter1) && !isNaN(quantity1) && !isNaN(diameter2) && !isNaN(quantity2) && quantity1 > 0 && quantity2 > 0 && diameter1 > 0 && diameter2 > 0) {
+        if (excludeCrust) {
+            diameter1 = diameter1 - 2;
+            diameter2 = diameter2 - 2;
+        }
+
         // Calculate the total areas of the pizzas
         const area1 = Math.PI * Math.pow(diameter1 / 2, 2) * quantity1;
         const area2 = Math.PI * Math.pow(diameter2 / 2, 2) * quantity2;
@@ -16,9 +24,9 @@ function compareAreas() {
         // Determine which pizza is bigger
         let biggerPizza = "";
         if (area1 > area2) {
-            biggerPizza = "The first pizza is bigger";
+            biggerPizza = "Pizza A is bigger";
         } else if (area1 < area2) {
-            biggerPizza = "The second pizza is bigger";
+            biggerPizza = "Pizza B is bigger";
         } else {
             biggerPizza = "Both pizzas have the same total area";
         }
@@ -27,14 +35,12 @@ function compareAreas() {
         const percentageDifference = ((area1 - area2) / area2) * 100;
 
         // Display the result with both areas, quantities, and friendlier percentage difference
-        let resultMessage = `<strong>${biggerPizza}.</strong><br>Total area of Pizza A: ${area1.toFixed(0)} square inches (Quantity: ${quantity1}).<br>Total area of Pizza B: ${area2.toFixed(0)} square inches (Quantity: ${quantity2}).<br>`;
+        let resultMessage = `<h3>${biggerPizza}.</h3><p>Total area of Pizza A: ${area1.toFixed(0)} square inches (Quantity: ${quantity1}).<br>Total area of Pizza B: ${area2.toFixed(0)} square inches (Quantity: ${quantity2}).<br>`;
         
         if (percentageDifference > 0) {
-            resultMessage += `Pizza A is <strong>${percentageDifference.toFixed(0)}% bigger</strong> than Pizza B.`;
+            resultMessage += `Pizza A is <strong>${percentageDifference.toFixed(0)}% bigger</strong> than Pizza B.</p>`;
         } else if (percentageDifference < 0) {
-            resultMessage += `Pizza A is <strong>${Math.abs(percentageDifference).toFixed(0)}% smaller</strong> than Pizza B.`;
-        } else {
-            resultMessage += "Pizzas A and B have the same total area.";
+            resultMessage += `Pizza A is <strong>${Math.abs(percentageDifference).toFixed(0)}% smaller</strong> than Pizza B.</p>`;
         }
 
         document.getElementById("result").innerHTML = resultMessage;
@@ -45,7 +51,7 @@ function compareAreas() {
 }
 
 // Add event listener to update the result on input change
-document.querySelector('.container').addEventListener('input', compareAreas);
+document.querySelector('body').addEventListener('input', compareAreas);
 
 // Initial comparison on page load
 compareAreas();
